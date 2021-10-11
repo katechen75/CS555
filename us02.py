d836f99da2cf77a22cbab9c8fa8fabe56a200e9d
katechen75 <43543679+katechen75@users.noreply.github.com>
2021-10-11 19:03:28 -0400

    updated us02 with test cases

--- us02.py
+++ us02.py
@@ -4,25 +4,46 @@
 from project03 import *
 import sys
 import datetime
+import unittest
 
 file_name = sys.argv[1]
 info = get_info(file_name)
 
-for family in info['families']:	
-    if family['married'] != None:
-        married = family['married'].split(" ")
-        mday = int(married[0])
-        mmonth = month_to_num[married[1]]
-        myear = int(married[2])
-        m = datetime.datetime(myear, mmonth, mday)
-        wbirth = None
-        hbirth = None
-        for individual in info['individuals']:
-            if individual["id"] == family['wife_id']:
-                wbirth = individual['birthday']
-            elif individual["id"] == family['husband_id']:
-                hbirth = individual['birthday']
+def formatMarriedDate(date):
+    married = date.split(" ")
+    mday = int(married[0])
+    mmonth = month_to_num[married[1]]
+    myear = int(married[2])
+    m = datetime.datetime(myear, mmonth, mday)
+    return m
 
-        if hbirth != None and wbirth != None:
-            if (hbirth > m) or (wbirth > m):
-                print("ERROR: FAMILY: US02: " + family['id'] + ": Marriage " + m.strftime("%m-%d-%Y") + " occurs before birthdays of both spouses")
+def birthBeforeMarriage():
+    def __init__(self):
+        for family in info['families']:	
+            if family['married'] != None:
+                # fixed smelly code
+                m = formatMarriedDate(family['married'])
+                wbirth = None
+                hbirth = None
+                for individual in info['individuals']:
+                    if individual["id"] == family['wife_id']:
+                        wbirth = individual['birthday']
+                    elif individual["id"] == family['husband_id']:
+                        hbirth = individual['birthday']
+
+                if hbirth != None and wbirth != None:
+                    if (hbirth > m) or (wbirth > m):
+                        return print("ERROR: FAMILY: US02: " + family['id'] + ": Marriage " + m.strftime("%m-%d-%Y") + " occurs before birthdays of both spouses")
+
+print(formatMarriedDate('3 JAN 1957'))
+
+class Testing(unittest.TestCase):
+    print('test 1: birth before marriage - assertNotEqual')
+    def test_marriedFam(self):
+        self.assertNotEqual(birthBeforeMarriage(), 123)
+
+    print('test 2: birth before marriage - assertNotEqual')
+    def test_marriedFam1(self):
+        self.assertNotEqual(birthBeforeMarriage(), int('19570401'))
+if __name__ == '__main__':
+    unittest.main(argv=[sys.argv[0]])
