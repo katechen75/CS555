02997113d0c0c2145e12d2d8a8ed40111cb6cbdd
Taylor Niedzielski <44853846+tniedzie@users.noreply.github.com>
2021-11-19 16:35:57 -0500

    Add files via upload

---
+++ us23.py
@@ -0,0 +1,23 @@
+## all individuals id should be different as well as birthdays
+## all the family id shoud be unique
+from project03 import *
+import sys
+import datetime
+import unittest
+from unittest.mock import patch
+from io import StringIO
+
+
+def unique_ids():
+	file_name = sys.argv[1]
+	info = get_info(file_name)
+	ret = 0
+	for ind1 in range(0, len(info['individuals'])):
+		for ind2 in range(1, len(info['individuals'])):
+            if (ind1['id'] == ind2['id']) & (ind1['birthday'] == ind2['birthday']) & 
+                (ind1['name'] == ind2['name']):
+				print("ERROR: FAMILY: US28: " + ind1['id'] + ", " + ind2['id'] + " have the same name and birthday")
+                ret =1
+    return ret
+
+
